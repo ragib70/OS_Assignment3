51b6e256d312a28532cc96e6284d91fba9d41e0f
ragib70 <ragib.hussain70@gmail.com>
2020-04-11 12:55:10 +0530

    New added without ageing

---
+++ Priority-Prempt.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct PCB{
+  int PID=0;
+  int burst=0;
+  int priority=0;
+  int arrival=0;// Time Instant when a process arrives.
+  int turnaround=0;// Time taken for the completion of the process from the instant it was submitted.
+  int wait=0;//Time spent waiting in the ready queue.
+  int completion=0;// Timestamp at which a particular process execution completes.###from the point of view of CPU.
+};
+
+bool comparetwoP(PCB P1, PCB P2){
+  if(P1.arrival == P2.arrival)
+    return (P1.priority < P2.priority);
+
+  return (P1.arrival < P2.arrival);
+}
+
+void fillWaitTime(PCB* P, int N){
+  int remaining[N];
+
+  for(int i=0; i<N; i++){
+    remaining[i] = P[i].burst;
+  }
+
+  int completed=0, minPriority=INT_MAX, process=0;
+
+  for(int time=P[0].arrival; completed!=N; time++){
+
+    int i=0;
+    int token = 0;
+    for(i=0; i<N; i++){
+      if(P[i].arrival<=time && remaining[i]>0 && P[i].priority<minPriority){
+          process=i;
+          minPriority = P[process].priority;
+          token = 1;
+        }
+    }
+
+    if(token==0){//Earlier I kept i==N and it failed make it sure to correct if any test case fails.
+      cout<<"At t = "<<time<<" , CPU is idle"<<endl;
+      continue;
+    }
+
+    cout<<"At t = "<<time<<", Highest Priority Job Till Now Executing = "<<P[process].PID<<endl;
+
+    remaining[process]--;
+
+    minPriority = INT_MAX;
+
+    if(remaining[process]==0){
+      cout<<"----------XXX----------Job "<<P[process].PID <<" Completed----------XXX-----------"<<endl;
+      completed+=1;
+      P[process].completion = time+1;
+      P[process].wait = P[process].completion - P[process].burst - P[process].arrival;
+      if(P[process].wait<0)
+        P[process].wait=0;
+
+    }
+
+  }
+
+}
+
+void findturnaroundtime(PCB* P,int N){
+  for(int i=0; i<N; i++){
+    P[i].turnaround = P[i].wait + P[i].burst;
+  }
+}
+
+float findaveragewait(PCB* P,int N){
+  float sum=0;
+  for(int i=0; i<N; i++){
+    sum+=P[i].wait;
+  }
+  return (sum/N);
+}
+
+float findaverageturnaround(PCB* P,int N){
+  float sum=0;
+  for(int i=0; i<N; i++){
+    sum+=P[i].turnaround;
+  }
+  return (sum/N);
+}
+
+int main(){
+
+  cout<<"Enter the No. of Processes ";
+  int N;
+  cin>>N;
+  struct PCB* P = new PCB[N];
+  cout<<"PID\t\tArrival Time\t\tBurst Time\t\tPriority"<<endl;
+  for(int i=0; i<N; i++){
+    cin>>P[i].PID>>P[i].arrival>>P[i].burst>>P[i].priority;
+  }
+
+  sort(P,P+N,comparetwoP);
+
+  cout<<"Simulation Results :"<<endl;
+  fillWaitTime(P,N);
+
+  findturnaroundtime(P,N);
+
+  cout<<"Process ID\t\tBurst Time\t\tPriority\t\tWait Time\t\tTurnaround Time\t\tCompletion Time"<<endl;
+  for(int i=0; i<N; i++){
+    cout<<P[i].PID<<"\t\t\t"<<P[i].burst<<"\t\t\t"<<P[i].priority<<"\t\t\t"<<P[i].wait<<"\t\t\t"<<P[i].turnaround<<"\t\t\t"<<P[i].completion<<endl;
+  }
+
+  cout<<"Average Wait Time = "<<findaveragewait(P,N)<<endl;
+
+  cout<<"Average Turnaround Time = "<<findaverageturnaround(P,N)<<endl;
+
+  free(P);
+
+
+
+return 0;
+}
